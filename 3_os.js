79e643bf0c97bd95f451474ce38bd3b3abb530c9
Shri-RajKumar <blkhackr1j@gmail.com>
2024-08-31 11:59:49 +0530

    second commit for os info about system

---
+++ 3_os.js
@@ -0,0 +1,16 @@
+const { log } = require('console')
+const os = require('os')
+
+console.log(os.networkInterfaces());
+
+console.log(os.arch());
+
+console.log(os.platform());
+
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.userInfo());
+
+
+

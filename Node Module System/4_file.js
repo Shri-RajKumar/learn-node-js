c8214633131cebd336f509ef73764b63e170eef5
Shri-RajKumar <blkhackr1j@gmail.com>
2024-08-31 12:51:45 +0530

    fileststem file f2.js have  used read, write , append  method in f1, f2 {.txt } file..

--- Node Module System/4_file.js
+++ Node Module System/4_file.js
@@ -2,17 +2,20 @@
 
 const fs = require('fs')
 
-const readFile = fs.readFileSync('D:\\Work\\NODE JS\\Node Module System\\resource\\f1.txt')
+const readFile = fs.readFileSync(`D:\\Work\\NODE JS\\Node Module System\\resource\\f1.txt`)
 
 console.log("---------------file content : -----------------\n" + readFile);
 
-const writeFile = fs.writeFileSync("./resource/f2.txt" , "file system f2.txt file content.")
+const writeFile = fs.writeFileSync("./resource/f1.txt" , "file system f1.txt file content.")
 
 console.log("created and wirtten the file." + writeFile);
 
-const appendf2file = fs.appendFileSync("./resource/f2.txt" , "\n file aapend : file system f2.txt file content.")
+const appendf1file = fs.appendFileSync("./resource/f1.txt" , "\n file aapend : file system f1.txt file content.")
 
-console.log("created file f2 has now append some data : " + appendf2file);
+console.log("created file f1 has now append some data : " + appendf1file);
 
+
+if(fs.unlinkSync("\\resource\\f1.txt"))
+    console.log("f2.txt file deleted");
 
 

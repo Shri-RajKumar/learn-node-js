0ca1f3097e0da02f7a600fc7e1893bffb437287a
Shri-RajKumar <blkhackr1j@gmail.com>
2024-08-31 19:35:07 +0530

    NPM adduser , publish own npm file and used in testOwnPublishPkgOnNPM folder

---
+++ testOwnPublishPkgOnNPM/testOwnPublishFile.js
@@ -0,0 +1,14 @@
+const calc = require('scalercalcraj')
+
+console.log(`hello raj kr`);
+
+// let exc = `raj`;
+// console.log(exc);
+
+
+calc.addition(1,2);
+calc.substraction(14 , 2);
+calc.multiplication(14 , 2);
+calc.division(14 , 2);
+
+
